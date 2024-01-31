797547c1475c33bcbeb08ef5f15ffd0f4b49f539
HUDSONHUGHES23 <hud22hughes@gmail.com>
2024-01-31 15:53:33 -0700

    Added Login Controller and Login Model
    *dologin not working yet because some imports are not working on vsc that normally work on eclipse

---
+++ JavaIII/milestone/src/main/java/com/gcu/models/LoginModel.java
@@ -0,0 +1,23 @@
+package com.gcu.models;
+
+public class LoginModel {
+	private String username;
+	private String password;
+	
+    public LoginModel() {
+    }
+    
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+}
+
