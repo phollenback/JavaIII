e3e945e41f08a39f73d0e8b4c1428d6eeb9a5c52
Carter Henning <carterehenning@gmail.com>
2024-02-02 16:00:13 -0700

    changed-application-name
    name change

--- JavaIII/milestone/src/main/java/com/gcu/MilestoneApplication.java
+++ JavaIII/milestone/src/main/java/com/gcu/MilestoneApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Milestone {
+public class MilestoneApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Milestone.class, args);
+		SpringApplication.run(MilestoneApplication.class, args);
 	}
 
 }
