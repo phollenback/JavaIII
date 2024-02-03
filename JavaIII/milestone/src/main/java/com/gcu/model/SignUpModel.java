b2de422ca289b9d02c754ad871c15f5d11ac571c
phollenb <peyholle@gmail.com>
2024-02-03 00:36:05 -0700

    Created forms and adjusted layout

--- JavaIII/milestone/src/main/java/com/gcu/model/SignUpModel.java
+++ JavaIII/milestone/src/main/java/com/gcu/model/SignUpModel.java
@@ -1,5 +1,53 @@
 package com.gcu.model;
 
-public class SignUpModel {
+public class SignUpModel 
+{
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int phoneNumber;
+    private String username;
+    private String password;
     
+    public SignUpModel()
+    {
+
+    }
+    
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String userName) {
+        this.username = userName;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
