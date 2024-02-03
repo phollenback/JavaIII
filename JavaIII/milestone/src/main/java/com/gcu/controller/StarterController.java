1fe6bf643f8d81848521438081273747878f839b
HUDSONHUGHES23 <hud22hughes@gmail.com>
2024-02-03 00:30:28 -0700

    Added a starterTemplate for the login and signup pages
    * Feel free to add / make changes to the template. I just included a header and centered flexbox

--- JavaIII/milestone/src/main/java/com/gcu/controller/StarterController.java
+++ JavaIII/milestone/src/main/java/com/gcu/controller/StarterController.java
@@ -1,5 +1,6 @@
 package com.gcu.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class StarterController {
 
     @GetMapping("/")
-    public String showStarterPage() {
+    public String showStarterPage(Model model) {
+        model.addAttribute("title", "Portfol.io");
         return "starter";
     }
 }
