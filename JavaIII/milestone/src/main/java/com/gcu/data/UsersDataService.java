8dff16ec0c0022af89a2937f0f0a303018d1514a
HUDSONHUGHES23 <hud22hughes@gmail.com>
2024-03-06 16:18:02 -0700

    Add DAO and Spring Data Services for Posts and Users

---
+++ JavaIII/milestone/src/main/java/com/gcu/data/UsersDataService.java
@@ -0,0 +1,50 @@
+package com.gcu.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.gcu.model.SignUpModel;
+
+public class UsersDataService<T> implements DataAccessInterface<T> {
+    @Autowired
+	private DataSource dataSource;
+	private JdbcTemplate jdbcTemplateObject;
+	
+	public UsersDataService(DataSource dataSource) {
+		this.dataSource = dataSource;
+		this.jdbcTemplateObject = new JdbcTemplate(dataSource);
+	}
+	 
+	@Override
+    public List<T> findAll() {
+        String sql = "SELECT * FROM ORDERS";
+        List<T> posts = new ArrayList<>();
+        return posts;
+    }
+	
+	@Override
+    public T findById(int id) {
+        return null;
+    }
+
+	@Override
+    public boolean create(T posts) {
+        
+        return false;
+    }
+
+	@Override
+	public boolean update(T t) {
+		return false;
+	}
+
+	@Override
+	public boolean delete(T t) {
+		return false;
+	}
+}
