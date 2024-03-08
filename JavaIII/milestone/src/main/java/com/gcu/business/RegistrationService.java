8bc90746778dee7d5afda1545279849b3ab3b8a2
phollenb <peyholle@gmail.com>
2024-03-08 16:14:46 -0700

    Logic for Registration Service

--- JavaIII/milestone/src/main/java/com/gcu/business/RegistrationService.java
+++ JavaIII/milestone/src/main/java/com/gcu/business/RegistrationService.java
@@ -1,23 +1,37 @@
 package com.gcu.business;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.gcu.data.UsersDataService;
 import com.gcu.model.SignUpModel;
 
 
 /**
  * The RegistrationService class provides methods for user registration.
  */
-public class RegistrationService {
-
+public class RegistrationService 
+{
+    @Autowired
+    private UsersDataService service;
     /**
      * Initializes a user.
      * 
      * @param user The SignUpModel object representing the user to be initialized.
      * @return true if the user is successfully initialized, false otherwise.
      */
-    public boolean initializeUser(SignUpModel user) {
-        // verify in database, eventually
-        if (user.getEmail().equals("generic.email.com")) {
-            return false;
+    public boolean initializeUser(SignUpModel user)
+     {
+        List<SignUpModel> users = service.findAll();
+
+        for(SignUpModel signUp : users)
+        {
+            if(user.getEmail().equals(signUp.getEmail()))
+            {
+                return false;
+            }
         }
+        // verify in database, eventually
         return true;
     }
     
--- JavaIII/milestone/src/main/java/com/gcu/data/UsersDataService.java
+++ JavaIII/milestone/src/main/java/com/gcu/data/UsersDataService.java
@@ -6,12 +6,16 @@ import java.util.List;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Service;
 
 import com.gcu.model.SignUpModel;
 
+@Service
 public class UsersDataService implements DataAccessInterface<SignUpModel> {
+   
     @Autowired
     private DataSource dataSource;
     private JdbcTemplate jdbcTemplateObject;
