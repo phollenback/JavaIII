9314dc9cedf4a26080d2d188e8366c6bfd497313
Carter Henning <carterehenning@gmail.com>
2024-03-08 21:59:32 -0700

    no-post-constructor
    got rid of premade posts

--- JavaIII/milestone/src/main/java/com/gcu/business/PostsBusinessService.java
+++ JavaIII/milestone/src/main/java/com/gcu/business/PostsBusinessService.java
@@ -20,23 +20,6 @@ public class PostsBusinessService implements PostServiceInterface
     @Autowired
     private DataAccessInterface<PostModel> service;
 
-
-    /**
-     * Initializes the list of posts with some default values.
-     */
-    public PostsBusinessService() {
-        // Initialize the list of posts
-        posts.add(new PostModel("https://via.placeholder.com/300", "POST 1", "this is filler text about the post.", "January 1, 2023"));
-        posts.add(new PostModel("https://via.placeholder.com/300", "POST 2", "this is filler text about the post.", "January 2, 2023"));
-        posts.add(new PostModel("https://via.placeholder.com/300", "POST 3", "this is filler text about the post.", "January 3, 2023"));
-        posts.add(new PostModel("https://via.placeholder.com/300", "POST 4", "this is filler text about the post.", "January 4, 2024"));
-        posts.add(new PostModel("https://via.placeholder.com/300", "POST 5", "this is filler text about the post.", "January 5, 2025"));
-        posts.add(new PostModel("https://via.placeholder.com/300", "POST 6", "this is filler text about the post.", "January 6, 2024"));
-        posts.add(new PostModel("https://via.placeholder.com/300", "POST 7", "this is filler text about the post.", "January 7, 2024"));
-        posts.add(new PostModel("https://via.placeholder.com/300", "POST 8", "this is filler text about the post.", "January 8, 2024"));
-    }
-
-
     /**
      * Retrieves the list of all posts.
      * @return the list of posts
